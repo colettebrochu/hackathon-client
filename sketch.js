eba8400b377a7de6892ba7945d7e9eb4e51e71ed
colettebrochu <123673109+colettebrochu@users.noreply.github.com>
2024-01-17 16:02:50 +0000

    stuff

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let object
+let value
+
 function setup () {
   // put setup code here
+  createCanvas(500, 500);
 }
 
 function draw () {
   // put drawing code here
+background(150);
+if (propClicked === true){
+  rect(propX, propY)
+}
+
 }
